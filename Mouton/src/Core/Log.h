1264947f8970984816cfa3554e1e08474c63ed1e
Corentin <coco41.cm@gmail.com>
2020-04-06 19:21:09 +0200

    Make logs macro aligned

--- Mouton/src/Core/Log.h
+++ Mouton/src/Core/Log.h
@@ -20,8 +20,8 @@ namespace Mouton
 }
 
 #define MTN_TRACE(...) ::Mouton::Log::GetLogger()->trace(__VA_ARGS__);
-#define MTN_INFO(...) ::Mouton::Log::GetLogger()->info(__VA_ARGS__);
-#define MTN_WARN(...) ::Mouton::Log::GetLogger()->warn(__VA_ARGS__);
+#define MTN_INFO(...)  ::Mouton::Log::GetLogger()->info(__VA_ARGS__);
+#define MTN_WARN(...)  ::Mouton::Log::GetLogger()->warn(__VA_ARGS__);
 #define MTN_ERROR(...) ::Mouton::Log::GetLogger()->error(__VA_ARGS__);
 #define MTN_FATAL(...) ::Mouton::Log::GetLogger()->critical(__VA_ARGS__);
 
